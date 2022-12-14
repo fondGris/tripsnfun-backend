6d3d3a50ba8c28d75e1e97b447792e51323bdc77
Farouk Himeur <fhimeur@gmail.com>
2022-12-14 15:48:18 +0100

    update: arrow added in signuScreen

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
-
+// const connectionString = process.env.CONNECTION_STRING;
+const connectionString = "mongodb+srv://admin:iFPu8y8B6Ca1K22C@atlascluster.rtt2tcs.mongodb.net/tripsnfun"
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
