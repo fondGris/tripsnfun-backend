bff85866f54b56cf07a9a37382a816085efd6bc3
JulienQ <jquan84@gmail.com>
2022-12-14 11:38:45 +0100

    route marker

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
+require('../models/connection');
+const Marker = require('../models/markers');
 
+router.post('/markers', (req, res) => {
+    const { token, userName, city, latitude, longitude } = req.body;
+    const newMarker = new Marker({ token, userName, city, latitude, longitude });
 
+    newMarker.save().then(() => {
+        res.json({ result: true });
+    });
+});
 
 module.exports = router;
