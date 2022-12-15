66922c3895011464a4e7c00363584198d5183a02
JulienQ <jquan84@gmail.com>
2022-12-15 17:53:01 +0100

    commentaire pour la route marker

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 require("../models/connection");
 const Marker = require("../models/markers");
 
+// route pour crée et gardé une localisation d un utilisateur, si c'etait null,
 router.post("/markers", (req, res) => {
   const { token, username, latitude, longitude } = req.body;
   Marker.findOne({ token }).then((data) => {
@@ -14,10 +15,11 @@ router.post("/markers", (req, res) => {
         longitude,
         isConnected: true,
       });
-
+// si route marker inexistant, il crée dans la base de données un nouveau marker
       newMarker.save().then(() => {
         res.json({ result: true });
       });
+      // il met à jour le token en true, si le user se connecte
     } else {
       Marker.updateOne({
         token: token,
@@ -30,7 +32,7 @@ router.post("/markers", (req, res) => {
     }
   });
 });
-
+// route pour rechercher tous les markers de la base de données
 router.get("/getMarkers", (req, res) => {
   Marker.find().then((data) => {
     if (data) {
@@ -40,7 +42,7 @@ router.get("/getMarkers", (req, res) => {
     }
   });
 });
-
+// route pour la deconnexion, changement du token a false.
 router.put("/status/:token", (req, res) => {
   Marker.updateOne({ token: req.params.token, isConnected: false }).then(
     (data) => {
